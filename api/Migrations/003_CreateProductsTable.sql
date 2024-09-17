125b1f549b3a719cb1c88bfd7078b1b7dd8a60d0
Võ Công Sinh <congsinhv@gmail.com>
2024-09-17 21:24:08 +0700

    Feature/add create noir schema (#10)
    * feat: Add Invoices table to the database
    
    * feat: Create noir schema and accounts table
    
    The code changes include creating a new schema called "noir" and adding the "Accounts" table to the schema. This commit message follows the convention of starting with a type prefix, in this case "feat" for a new feature.
    
    * chore: Update column name from "name" to "prod_name" and "depiction" to "prod_desc" in Products table

--- api/Migrations/003_CreateProductsTable.sql
+++ api/Migrations/003_CreateProductsTable.sql
@@ -2,9 +2,9 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS noir.Products  (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name VARCHAR(255) NOT NULL,
-    desc TEXT,
-    price INT NOT NULL, 
+    prod_name VARCHAR(255) NOT NULL,
+    prod_desc TEXT,
+    price INT NOT NULL,
     inventory INT NOT NULL,
     image_url TEXT,
 );
