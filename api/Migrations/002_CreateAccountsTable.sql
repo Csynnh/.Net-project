14cf13e5940a85f54469855f48609d073155c122
phamlucchuong <lucchuongg@gmail.com>
2024-09-17 20:47:56 +0700

    change sth:)

--- api/Migrations/002_CreateAccountsTable.sql
+++ api/Migrations/002_CreateAccountsTable.sql
@@ -1,5 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS noir.Accounts (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     name VARCHAR(255),
