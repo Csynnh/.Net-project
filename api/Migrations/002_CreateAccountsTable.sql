65c4d9fc835683212e67ca1fe5046a727405bffa
phamlucchuong <lucchuongg@gmail.com>
2024-09-17 20:45:13 +0700

    change serial to uuid in 002_ file

--- api/Migrations/002_CreateAccountsTable.sql
+++ api/Migrations/002_CreateAccountsTable.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS noir.Accounts (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     name VARCHAR(255),
