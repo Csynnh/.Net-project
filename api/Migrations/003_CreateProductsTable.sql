a1b9a475e588762ed407f66bd4815b52c89598a6
BuiHoanDuy <hoanduy1802@gmail.com>
2024-09-17 20:38:17 +0700

    tao bang Products

--- api/Migrations/003_CreateProductsTable.sql
+++ api/Migrations/003_CreateProductsTable.sql
@@ -1,5 +1,7 @@
-CREATE TABLE IF NOT EXISTS noir.Products (
-    id SERIAL PRIMARY KEY,
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS noir.Products  (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255) NOT NULL,
     desc TEXT,
     price INT NOT NULL, 
