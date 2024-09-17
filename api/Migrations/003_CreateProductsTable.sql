a0b428fe596e4e8b5fe34e0e6a9f74021ac78487
Csynnh <congsinhv@gmail.com>
2024-09-17 21:23:07 +0700

    chore: Update column name from "name" to "prod_name" and "depiction" to "prod_desc" in Products table

--- api/Migrations/003_CreateProductsTable.sql
+++ api/Migrations/003_CreateProductsTable.sql
@@ -2,8 +2,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS noir.Products  (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name VARCHAR(255) NOT NULL,
-    depiction TEXT,
+    prod_name VARCHAR(255) NOT NULL,
+    prod_desc TEXT,
     price INT NOT NULL,
     inventory INT NOT NULL,
     image_url TEXT,
