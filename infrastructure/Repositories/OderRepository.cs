6fcc5eb8410d25c36de498b1257a346625fef611
Csynnh <congsinhv@gmail.com>
2024-11-17 16:03:06 +0700

    Update order status in INSERT query from 'PREPARING' to 'CONFIRMING' in OderRepository

--- infrastructure/Repositories/OderRepository.cs
+++ infrastructure/Repositories/OderRepository.cs
@@ -152,7 +152,7 @@ public class OderRepository : IOderRepository
     {
         var sql = $@"
             INSERT INTO DEV.ORDERS (account_id, total, payment_method_id, shipping_method_id, stored_information_id, status)
-            VALUES (@accountId, @total, @paymentMethodId, @shippingMethodId, @storedInformationId, 'PREPARING')
+            VALUES (@accountId, @total, @paymentMethodId, @shippingMethodId, @storedInformationId, 'CONFIRMING')
             RETURNING id, account_id, total, payment_method_id, shipping_method_id, stored_information_id, status;
         ";
         using var conn = _dataSource.OpenConnection();
