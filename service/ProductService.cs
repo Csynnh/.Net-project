e54ca2c7217ecb38e4f0872eb3e798c7a88ccccd
HuynhThiTrucLam <httlam2004ntkt@gmail.com>
2024-12-12 11:17:03 +0700

    Feature: Update product management with new endpoints and improve database schema

--- service/ProductService.cs
+++ service/ProductService.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using infrastructure.DataModels;
 using infrastructure.QueryModels;
 using infrastructure.Repositories;
+using Microsoft.AspNetCore.Http;
 
 namespace service;
 
@@ -13,6 +14,8 @@ public interface IProductService
     Task<PagedResponse<ProductModelResponse>> ListProductByTypeNameAsync(string name, int pageNumber, int pageSize, string? size, decimal? minPrice, decimal? maxPrice);
     Task<IEnumerable<ListProductByOderStatusResponse>> ListProductByOderStatusAsync(Guid accountId, string orderStatus);
     Task<IEnumerable<ListProductByTypeResponse>> ListProductByTypeAsync();
+    Task<string> UpdateProductAsync(Guid id, UpdateProductModel ProductRequest);
+    Task<string> UploadImageAsync(IFormFile image);
 }
 
 
@@ -60,12 +63,44 @@ public class ProductService : IProductService
         try
         {
             // Check if the product already exists based on your criteria
-            var existingProduct = await _repository.IsProductExistAsync(ProductRequest.ProductName, ProductRequest.Color, ProductRequest.Size);
-            if (existingProduct)
-            {
-                throw new InvalidOperationException($"Product with name: '{ProductRequest.ProductName}' - color: '{ProductRequest.Color}' - size '{ProductRequest.Size}' already exists");
-            }
+            var existingProduct = await _repository.GetProductByNameColorSizeAsync(ProductRequest.ProductName, ProductRequest.Color, ProductRequest.Size);
             var uploader = new BlodUploader();
+            if (existingProduct != null)
+            {
+                // If the product already exists, update the adding inventory
+                dynamic existingProductDynamic = existingProduct.Variants;
+                System.Console.WriteLine($"{existingProduct.Id} -- Product already exists: {existingProductDynamic[0].Id}");
+                var Inventory = existingProductDynamic[0].Inventory;
+                var newInventory = Inventory + ProductRequest.Inventory;
+                string imgThubnail = await uploader.UploadFileAsync(ProductRequest.Images.ImageThumbnail);
+                List<string> updatedAdditionalImageUrls = new List<string>();
+                foreach (var additionalImage in ProductRequest.Images.AdditionalImages)
+                {
+                    updatedAdditionalImageUrls.Add(await uploader.UploadFileAsync(additionalImage));
+                }
+
+                string updatedImages = JsonSerializer.Serialize(new ProductImagesModel()
+                {
+                    ImageThumbnail = imgThubnail,
+                    AdditionalImages = updatedAdditionalImageUrls
+                }
+                );
+
+                var newProduct = new ProductModel()
+                {
+                    Name = ProductRequest.ProductName,
+                    Description = ProductRequest.ProductDescription,
+                    Size = ProductRequest.Size.ToUpper(),
+                    Color = ProductRequest.Color.ToUpper(),
+                    Type = ProductRequest.Type.ToUpper(),
+                    Price = ProductRequest.Price,
+                    Inventory = newInventory,
+                    Details = JsonSerializer.Serialize(ProductRequest.Details),
+                    Images = updatedImages
+                };
+                await _repository.UpdateProductAsync(existingProduct.Id, existingProductDynamic[0].Id, newProduct);
+                return $"Product {ProductRequest.ProductName} updated successfully with new inventory {newInventory}!";
+            }
             string image_url = await uploader.UploadFileAsync(ProductRequest.Images.ImageThumbnail);
             List<string> additionalImageUrls = new List<string>();
             foreach (var additionalImage in ProductRequest.Images.AdditionalImages)
@@ -102,6 +137,58 @@ public class ProductService : IProductService
         {
 
             throw new InvalidOperationException($"An error occurred while creating the product: {ex.Message}");
+        }
+    }
+
+    public async Task<string> UpdateProductAsync(Guid id, UpdateProductModel ProductRequest)
+    {
+        try
+        {
+
+            var existingProduct = await _repository.GetProductByIdAsync(id);
+            if (existingProduct == null)
+            {
+                throw new InvalidOperationException($"Product with id: '{id}' does not exist");
+            }
+            System.Console.WriteLine($"Product found: {JsonSerializer.Serialize(ProductRequest.Images)}");
+            string images = JsonSerializer.Serialize(new ProductImagesModel()
+            {
+                ImageThumbnail = ProductRequest.Images.ImageThumbnail,
+                AdditionalImages = ProductRequest.Images.AdditionalImages
+            });
+
+            ProductModel productModel = new ProductModel()
+            {
+                Name = ProductRequest.ProductName,
+                Description = ProductRequest.ProductDescription,
+                Size = ProductRequest.Size.ToUpper(),
+                Color = ProductRequest.Color.ToUpper(),
+                Type = ProductRequest.Type.ToUpper(),
+                Price = ProductRequest.Price,
+                Inventory = ProductRequest.Inventory,
+                Details = JsonSerializer.Serialize(ProductRequest.Details),
+                Images = images
+            };
+
+            await _repository.UpdateProductAsync(existingProduct.Id, id, productModel);
+            return $"Product {ProductRequest.ProductName} updated successfully!";
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidOperationException($"An error occurred while updating the product: {ex.Message}");
+        }
+    }
+
+    public async Task<string> UploadImageAsync(IFormFile image)
+    {
+        try
+        {
+            var uploader = new BlodUploader();
+            return await uploader.UploadFileAsync(image);
+        }
+        catch (System.Exception ex)
+        {
+            throw new InvalidOperationException($"An error occurred while uploading the image: {ex.Message}");
         }
     }
 
